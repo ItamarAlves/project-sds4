43d472190a67852fcd6d9f82a77cb6ec99b622df
ItamarAlves <itamaralvesprofi@gmail.com>
2021-09-10 00:54:53 -0400

    feat: project  created

---
+++ backend/src/test/java/com/alves/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alves.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
